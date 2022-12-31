1c25fb2d408c107330202d7e2ea593a577edbe56
Kurosvet <s.l.kurova@gmail.com>
2022-12-31 20:27:50 +0300

    NTC-1-godfather added jms for command  pom

--- nutcracker/godfather/src/main/java/com/slekupuh/mts/nutcraker/godfather/StartPerformanceCommandHandler.java
+++ nutcracker/godfather/src/main/java/com/slekupuh/mts/nutcraker/godfather/StartPerformanceCommandHandler.java
@@ -3,14 +3,24 @@ package com.slekupuh.mts.nutcraker.godfather;
 
 import com.slekupuh.mts.nutcraker.core.command.CommandHandler;
 import com.slekupuh.mts.nutcraker.godfather.command.StartPerformanceCommand;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class StartPerformanceCommandHandler extends CommandHandler<StartPerformanceCommand> {
     @Override
     @JmsListener(destination = "godfatherCommand", containerFactory = "commandFactory")
     public void handle(StartPerformanceCommand command) {
+        log.debug(" command {} processId {}",command.getClass().getSimpleName(), command.getProcessId());
         // todo:next
     }
+
+    //JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+    //
+    //    // Send a message with a POJO - the template reuse the message converter
+    //    System.out.println("Sending an email message.");
+    //    jmsTemplate.convertAndSend("mailbox", new Email("info@example.com", "Hello"));
+    //  }
 }
--- nutcracker/godfather/src/test/java/com/slekupuh/mts/nutcraker/godfather/StartPerformanceCommandHandlerTest.java
+++ nutcracker/godfather/src/test/java/com/slekupuh/mts/nutcraker/godfather/StartPerformanceCommandHandlerTest.java
@@ -1,10 +1,16 @@
 package com.slekupuh.mts.nutcraker.godfather;
 
 import com.slekupuh.mts.nutcraker.godfather.command.StartPerformanceCommand;
+import com.slekupuh.mts.nutcraker.godfather.config.JMSTestConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.UUID;
-
+//@RunWith(SpringRunner.class)
+@SpringBootTest
+@Import(JMSTestConfig.class)
 public class StartPerformanceCommandHandlerTest {
     private StartPerformanceCommandHandler handler;
 
